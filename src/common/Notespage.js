739a6a4d8f63a47ea4e2b56eb217adfcefbe7c6e
Jyothsna <68052580+jyothsnamanchiraju@users.noreply.github.com>
2021-11-10 19:05:21 +0530

    code refactoring
    removed the functions for FavList, NotesList, AddNewNote, TrashList and notesDisplay .js files and included them as constants in render method of Notepage.js

--- src/common/Notespage.js
+++ src/common/Notespage.js
@@ -6,17 +6,14 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem'; 
 import ListItemButton from '@mui/material/ListItemButton';  
 import ListItemText from '@mui/material/ListItemText'; 
-
-import AddNewNote from '../assets/MenuOptions/AddNewNote'; 
-import FavList from '../assets/MenuOptions/FavList'; 
-import NotesList from '../assets/MenuOptions/NotesList'; 
-import TrashList from '../assets/MenuOptions/TrashList'; 
+import { TextareaAutosize } from '@material-ui/core';
 
 class Notespage extends Component{
     constructor(){
         super(); 
         this.state={
             listItemSelect:"", 
+            noteSelected:0,
             notes:[{
                     id:1, 
                     text:"corn**",
@@ -44,12 +41,88 @@ class Notespage extends Component{
                 ]
         }
     }
-
+    
     listItemHandler=()=>{
-            this.setState({listItemSelect:"list-item-select"}); 
+        this.setState({listItemSelect:"list-item-select"}); 
     }
 
     render(){
+       // let nts = this.state.notes; 
+        const AddNewNote=(
+            <div>
+            <List>
+             <ListItem disablePadding className="newNote">
+                <ListItemButton style={{height:'50px'}}>
+                    <ListItemText primary="New Notes" />
+                </ListItemButton>
+             </ListItem>
+            </List>
+        </div>
+        );
+
+        const NotesList =(
+            <div>
+            <List>{
+                    this.state.notes.filter(n1=>(n1.category!=='trash')).map((n2)=>
+                        <ListItem disablePadding className="NotesList" 
+                                  onClick={()=>{this.setState({noteSelected: n2.id})}}>
+                            <ListItemButton style={{height:'50px'}}>
+                                <ListItemText key={n2.id} primary={n2.text} />
+                            </ListItemButton>
+                        </ListItem>
+                    )
+                }
+            </List>
+        </div>
+        );
+
+        const FavList=(
+        <div>
+            <List>{
+                    this.state.notes.filter(n=>(n.favourite === true && n.category!=='trash')).map((f)=>(
+                    <ListItem disablePadding className="Favorites"
+                              onClick={()=>{this.setState({noteSelected: f.id})}}  >
+                        <ListItemButton style={{height:'50px'}}>
+                            <ListItemText key ={f.id} primary={f.text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))
+                }
+            </List>
+        </div>
+        );
+
+        const TrashList=(
+        <div>
+                <List>
+                    { this.state.notes.filter(n=>(n.category ==='trash')).map(t=>(
+                        <ListItem disablePadding className="Trash"
+                                  onClick={()=>{this.setState({noteSelected: t.id})}} >
+                            <ListItemButton style={{height:'50px'}}>
+                                <ListItemText key={t.id} primary={t.text} />
+                            </ListItemButton>
+                        </ListItem>
+                        ))
+                    }
+                </List>
+        </div>
+        );
+
+        const notesDisplay=(
+           <div> 
+            {
+            this.state.notes.filter(n=>(n.id === this.state.noteSelected)).map(s=>(
+            <TextareaAutosize aria-label="scratch-area" placeholder="scratch pad"
+                                        size='large'
+                                        variant='filled'
+                                        style={{width:'1000px', border:'none'}}
+                                        value={s.text}
+                                        onChange={this.textChangeHandler}> 
+            </TextareaAutosize>
+            ))}
+            </div>
+        );
+
         return(
             <div style={{display:'flex', flexDirection:'row', width:'auto'}}>
                 <div className="notes-header">
@@ -66,32 +139,36 @@ class Notespage extends Component{
                         </List>
                         {this.props.type === 'new'?
                             <div>
-                                <AddNewNote/> 
-                                <NotesList/>
+                                {AddNewNote} 
+                                {NotesList}
                             </div>
                             : this.props.type ==='fav'?
                             <div>
-                                 <FavList nts={this.state.notes}/>
+                                 {FavList}
                             </div>
                             : this.props.type === 'trash'?
                             <div> 
-                                 <TrashList nts={this.state.notes}/>
+                                 {TrashList}
                             </div>
                             : 
                             <div>
-                                <NotesList nts={this.state.notes}/>
+                                {NotesList}
                             </div>
                         }
                     </div>
                 </div>
                 <div className="notes-content">
-                     
+                     {
+                         this.state.noteSelected ===0 ?
+                         <div> New Note</div>
+                         :
+                         <div>{notesDisplay}</div>
+                     }
                 </div>
 
             </div>
         )
     }
-
 }
 
 export default Notespage; 
